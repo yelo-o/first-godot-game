5f3b6931ab407bd1d9ca0f3bffb592a5328f1854
yelo-o <flyordig@gmail.com>
2025-01-07 23:13:04 +0900

    Add: Player's jump & run animation \n Update: time_scale reduced for a while and player queued free when got killed

--- beginner_tutorial/scripts/killzone.gd
+++ beginner_tutorial/scripts/killzone.gd
@@ -4,7 +4,10 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
